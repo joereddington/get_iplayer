7f2445801fc9857f54952ff006df74a1d0b7bd4c
Joe Reddington <joe@joereddington.com>
2015-08-31 13:25:49 +0100

    todo.py now uploads with the correct filename

--- todo.py
+++ todo.py
@@ -25,69 +25,68 @@ def enguage_auth():
         gauth.LocalWebserverAuth()
         return gauth
 
+
 def upload_csv(filename):
-    drive = GoogleDrive(enguage_auth())
-    file5 = drive.CreateFile()
-    # Read file and set it as a content of this instance.
-    file5.SetContentFile(filename)
-    file5.Upload({'convert':True})  # Upload it
+        drive = GoogleDrive(enguage_auth())
+        file5 = drive.CreateFile()
+        # Read file and set it as a content of this instance.
+        file5.SetContentFile(filename)
+        file5.Upload({'convert': True})  # Upload it
 
 
-def convert_srt_to_sup(filename):
-    subs = pysrt.open(filename)
-    with open("out.csv", "wb") as f:
-            writer = csv.writer(f)
-            writer.writerow(
-                ("Start",
-                 "End",
-                 "Character",
-                 "Original Text",
-                 "Translation",
-                 "Machine Translations"))
-            for caption in subs:
-                    writer.writerow(
-                        (caption.start,
-                         caption.end,
-                         "",
-                         caption.text,
-                         "",
-                         ""))
+def convert_srt_to_sup(input_file, out_file):
+        subs = pysrt.open(input_file)
+        with open(out_file, "wb") as f:
+                writer = csv.writer(f)
+                writer.writerow(
+                    ("Start",
+                     "End",
+                     "Character",
+                     "Original Text",
+                     "Translation",
+                     "Machine Translations"))
+                for caption in subs:
+                        writer.writerow(
+                            (caption.start,
+                             caption.end,
+                             "",
+                             caption.text,
+                             "",
+                             ""))
 
 
 
-#Here's what I want, I want to download subtitle files, convert them into CVS ones and make them ready to go up to Google Drive.
+URL = "http://www.bbc.co.uk/iplayer/episode/b068232r/secrets-of-china-1-fit-in-or-fail"
+name = URL.rpartition('/')[2]+".csv"
 
-#Let's work this out.
-URL="http://www.bbc.co.uk/iplayer/episode/b068232r/secrets-of-china-1-fit-in-or-fail"
-#Download: done.
 os.system("perl get_iplayer --force "+URL+" --modes=subtitles")
+# Download: done.
 
+# Convert to SRT
+convertSubtitles.ttml2srt(["lastdownloaded.srt"], False, False)
 
-#Convert to SRT
-convertSubtitles.ttml2srt(["lastdownloaded.srt"],False,False)
+# Convert into CSV (which I'm choosing to call *.Sup)
+convert_srt_to_sup('lastdownloaded_converted.srt', name)
+# Upload to google drive
+upload_csv(name)
 
-#Convert into CSV (which I'm choosing to call *.Sup)
-#filename is lastdownloaded_converted.srt'
-
-convert_srt_to_sup('lastdownloaded_converted.srt')
-#Upload to google drive
-upload_csv('out.csv')
 
 def test_cases_to_write():
-	"""
-	We need,
+        """
+        We need,
 
-	Several checks that we are downloading the correct file
-		find out if there is a file comparison bit of the unit testing
-	Check the empty file.
-	Check extremely long file (in terms of content, and of time passing)
-	Several checks that the file we convert matches our other examples.
-	Several checks that converting to *.sup format works
+        Several checks that we are downloading the correct file
+                find out if there is a file comparison bit of the unit testing
+        Check the empty file.
+        Check extremely long file (in terms of content, and of time passing)
+        Several checks that the file we convert matches our other examples.
+        Several checks that converting to *.sup format works
 
-	Several checks that the *.sup format, once downloaded, works with the rest of the framework.
-	A long form check, with an srt file going up, coming down and converting to english.
+        Several checks that the *.sup format, once downloaded, works with
+        the rest of the framework.
+        A long form check, with an srt file going up, coming down and
+        converting to english.
 
 
-	"""
-	pass
-
+        """
+        pass
